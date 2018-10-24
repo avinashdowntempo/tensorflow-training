10e883eeb91bc0c946776df1a4accadc9c1e8dc5
Jeyasankar <Avinash.Jeyasankar@CenturyLink.com>
2018-10-24 16:00:28 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var tf = require("@tensorflow/tfjs");
+tf.tensor([1, 2, 3, 4]).print();
---
+++ index.ts
@@ -0,0 +1,2 @@
+import * as tf from '@tensorflow/tfjs';
+tf.tensor([1, 2, 3, 4]).print();
